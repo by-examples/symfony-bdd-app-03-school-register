1679dc8c5e1c08a371ed8e1bd44deea414278594
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-02-05 14:01:55 +0100

    Kick travis

--- reload.bash
+++ reload.bash
@@ -14,6 +14,7 @@ fi
 php app/console cache:clear --env=prod
 php app/console cache:warmup --env=prod
 
+
 sudo mysql -u root < 00-extra/db/create-empty-database.sql
 php app/console doctrine:schema:update --force
 
