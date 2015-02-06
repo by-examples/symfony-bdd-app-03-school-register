fc2acad239e36fe4415d8dfd3c28dac50e70f8c4
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-02-06 13:06:51 +0100

    Minimize dependencies

--- puppet/manifest.pp
+++ puppet/manifest.pp
@@ -1,4 +1,7 @@
 class { 'symfony':
-    username => 'travis',
-    directory => '/home/travis/build/by-examples/symfony-bdd-app-03-school-register/web'
+    username        => 'travis',
+    directory       => '/home/travis/build/by-examples/symfony-bdd-app-03-school-register/web',
+    withEnvironment => false,
+    withNodejs      => false,
+    withAllPhars    => false,
 }
