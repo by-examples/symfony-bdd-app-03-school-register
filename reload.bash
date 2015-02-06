442e0a11a3cac5107ece46995b72124a0e06720d
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-02-06 13:31:02 +0100

    Reload should start with: create db, create user, flush priv

--- reload.bash
+++ reload.bash
@@ -11,12 +11,11 @@ then
     exit 1
 fi
 
+mysql -u root < 00-extra/db/create-empty-database.sql
+sleep 5
+
 php app/console cache:clear --env=prod
 php app/console cache:warmup --env=prod
 
-
-sudo mysql -u root < 00-extra/db/create-empty-database.sql
-sleep 5
 php app/console doctrine:schema:update --force
-
 php app/console fos:user:create admin admin@example.net loremipsum --super-admin
