86114ffd89801a9426dd9329204353c68852affb
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2015-02-09 13:37:00 +0100

    get the pwd dir by txt file

--- puppet/travis.pp
+++ puppet/travis.pp
@@ -2,14 +2,22 @@
 # It was not tested on other hosts
 #
 
-$tmp_pwd = generate('/bin/bash', '-c', 'eval echo "~travis"')
-$final_pwd = inline_template('<%= @tmp_pwd.strip %>')
+#$tmp_pwd = generate('/bin/bash', '-c', 'eval echo "~travis"')
+#$final_pwd = inline_template('<%= @tmp_pwd.strip %>')
+#notify { "FINAL PWD: ${final_pwd}": }
 
+
+#
+# The file travis-config.txt is created in .travis.yml
+#
+$tmp_pwd = generate('/bin/cat', 'travis-config.txt')
+$final_pwd = inline_template('<%= @tmp_pwd.strip %>')
 notify { "FINAL PWD: ${final_pwd}": }
 
 class { 'symfony':
     username        => 'travis',
     directory       => "${final_pwd}/web",
+#    directory       => '/home/travis/build/by-examples/symfony-bdd-app-03-school-register',
     withEnvironment => false,
     withNodejs      => false,
     withAllPhars    => false,
